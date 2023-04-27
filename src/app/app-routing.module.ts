69c01ea499ab2be8cf3d10a200b7feeddf8aab5e
Aero <khasa@orsted.com>
2023-04-27 20:02:55 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ServerListComponent } from './server-list/server-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'server-list', component: ServerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
